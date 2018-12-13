d647b25674d879add7380c1145e2be5af60581b2
Spiros Kabasakalis <kabasakalis@gmail.com>
2018-12-13 15:36:00 +0200

    configured workflow added tasks

--- tasks/cmake_config_debug.sh
+++ tasks/cmake_config_debug.sh
@@ -4,4 +4,4 @@ echo cmake -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug 
 # Configure with cmake for Debug build, and fix  compile_commands.json
 echo Directory: 
 pwd
-cmake -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug .. && ../bin/compile_commands_fix.sh
+cmake -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug .. && ../tasks/compile_commands_fix.sh
