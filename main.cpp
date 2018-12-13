b6165be4a4171f2f13473fdf3d1c6e1969a0e272
Spiros Kabasakalis <kabasakalis@gmail.com>
2018-12-13 02:35:59 +0200

    configured workflow

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
-
 /**
  * A Simple Hello World with SFML.
  *
- * Build: g++ -o sfml_hello sfml_hello.cpp -lsfml-graphics -lsfml-window -lsfml-system
+ * Build: g++ -o sfml_hello sfml_hello.cpp -lsfml-graphics -lsfml-window
+ * -lsfml-system
  */
 
 #include <SFML/Window.hpp>
+#include <vector>
 
 int main() {
-    sf::Window window(sf::VideoMode(640, 480), "Hello world!");
-    sf::sleep(sf::seconds(10));
+  sf::Window window(sf::VideoMode(640, 480), "Hello world!");
 
-    return 0;
+  sf::sleep(sf::seconds(10));
+  sf::Vector2f avector;
+  sf::Vector3<int> c;
+  std::vector<int> v;
+  avector.x = 10.0;
+
+  return 0;
 }
